1a5a43b790de993a064d8360000dbea6dec973fe
Варенцов Николай <n.varentsov@dmbasis.ru>
2020-11-09 12:21:59 +0300

    Examples consoleApp in VS

---
+++ Day_1/ConsoleApp_DotNetFramework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_DotNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Dotnet");
+            Console.ReadLine();
+        }
+    }
+}
