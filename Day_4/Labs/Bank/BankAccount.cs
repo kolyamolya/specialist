2838b7770271b4ad3d20688673bfed78f1ab1cc1
Варенцов Николай <n.varentsov@dmbasis.ru>
2020-11-12 21:20:56 +0300

    лабораторная Bank Account день 4

---
+++ Day_4/Labs/Bank/BankAccount.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+
+        public string Owner { get; private set; }
+        public Decimal Balance { get; private set; }
+
+        public event Action<string> DepositNotify;
+        public event Action<string> WithdrawNotify;
+
+        public BankAccount(string Owner)
+        {
+            this.Owner = Owner;
+        }
+
+        public void Deposit(Decimal summa) 
+        {
+
+            if (summa < 0)
+            {
+                DepositNotify?.Invoke($"{nameof(summa)} не может быть отрицательное число");
+            }
+            else
+            {
+                Balance += summa;
+
+                DepositNotify?.Invoke($"Вы успешно пополнили на сумму {summa}\n" +
+                    $"Ваш баланс равен {Balance}");
+            }
+        }
+
+        public bool Withdraw(Decimal summa)
+        {
+            if (Balance < summa)
+            {
+                WithdrawNotify?.Invoke($"Недостаточно средста для списания");
+                return false;
+            }
+            else 
+            {
+                Balance -= summa;
+                WithdrawNotify?.Invoke($"Произошло снятие средств в размере {summa}\n" +
+                    $"Ваш баланс равен {Balance}");
+
+                return true;
+            }
+        }
+
+        public static void Transfer(BankAccount source, BankAccount dest, Decimal summa) 
+        {
+            source.Withdraw(summa);
+            dest.Deposit(summa);
+        }
+
+        public override string ToString()
+        {
+            return $"Owner : {Owner}; Balance : {Balance}";
+        }
+
+    }
+}
---
+++ Day_4/Labs/Bank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount NickAccount = new BankAccount("Nick");
+
+            NickAccount.DepositNotify += PrintConsole;
+            NickAccount.WithdrawNotify += PrintConsole;
+
+            BankAccount SergeyAccount = new BankAccount("Sergey");
+
+            SergeyAccount.DepositNotify += PrintConsole;
+            SergeyAccount.WithdrawNotify += PrintConsole;
+
+            NickAccount.Deposit(100100);
+
+            BankAccount.Transfer(NickAccount, SergeyAccount, 29000);
+
+            Console.WriteLine(SergeyAccount);
+            Console.WriteLine(NickAccount);
+        }
+
+        public static void PrintConsole(String message) 
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
